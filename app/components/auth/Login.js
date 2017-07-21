e49e9260983b2e194f3d8e479546b6fd211fac50
ahang <hang.andy.t@gmail.com>
2017-07-20 21:49:30 -0500

    before redirecting on success login, remove successMessage local storage so it doesnt appear after the initial success reg

--- app/components/auth/Login.js
+++ app/components/auth/Login.js
@@ -38,6 +38,7 @@ class Login extends Component {
 	}).then((response) => {
 	  //console.log(JSON.stringify(response));
 	  Auth.authenticateUser(response.data.token);
+      localStorage.removeItem("successMessage");
 	  this.setState({ redirect: true });
 	})
   }
